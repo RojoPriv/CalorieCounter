8a48b4aac351a77299739325d80603dede8072ff
Jarvis . <elancheran55@hotmail.com>
2025-08-26 22:34:36 +0200

    Add project files.

---
+++ Pages/FoodInfo.xaml.cs
@@ -0,0 +1,14 @@
+namespace CalorieCounter.Pages;
+
+public partial class FoodInfo : ContentPage
+{
+	public FoodInfo()
+	{
+		InitializeComponent();
+	}
+
+    private void addButton_Clicked(object sender, EventArgs e)
+    {
+	
+    }
+}
